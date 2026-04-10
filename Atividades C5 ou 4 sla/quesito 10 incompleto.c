661da042897d71d78fd0cb4948dc0f35cbb50005
boredflush <tavinholuizmarqx@gmail.com>
2026-04-10 13:12:01 -0300

    Update quesito 10 incompleto.c

--- Atividades C5 ou 4 sla/quesito 10 incompleto.c
+++ Atividades C5 ou 4 sla/quesito 10 incompleto.c
@@ -1,13 +1,12 @@
-
 // Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
   
-    int idade=0,sexo=0,opiniao=0,cont;
-    float md1,md2,md3;
+    int idade=0,sexo=0,opiniao=0,cont,md1,md2,md3;
+
     
-    for(cont=1;cont<=15;cont++){
+    for(cont=1;cont<=2;cont++){
     
     printf("Digite idade:");
     scanf("%d",&idade);
@@ -16,15 +15,15 @@ int main() {
    scanf("%d",&sexo);
    
     printf("Digite opiniao (1-não gostei/2-esperava mais/3-vale o ingresso/4-Exelente) :");
-    scanf("%d",&opiniao)
+    scanf("%d",&opiniao);
     
     if(sexo==1&&opiniao==4){
-        
-        md1++;
     
-    }
-
+    md1=sexo+opiniao/2;
+        
+        printf("total de mulheres que acharam o filme excelente foi :%d",md1);
+    
 }
-
+    
     return 0;
 }
