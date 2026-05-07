d70e7aa8d859631cdeaa81b14c7a7331f3c7fbea
boredflush <tavinholuizmarqx@gmail.com>
2026-05-07 14:46:32 -0300

    Add files via upload

---
+++ desafio coding revisao.c
@@ -0,0 +1,85 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    //variaveis// 
+    
+    int p;
+    int tfilhos[1000], percentual;
+    float tsalario[1000],soma_salarios=0,soma_filhos;
+    float salario, mediasal=0, mediaf=0, mediafi=0;
+    int habit2fi=0, perc2=0;
+    float maior,menor;
+  
+  //for que vai repetir 1000 vezes 1 pra cada aluno//
+  
+ for (p=1; p<=3;p++){
+  
+  //salario dos alunos//
+  
+    printf("digite seu salário (0 se nao houver) :");
+    scanf("%f",&tsalario[p]);
+  
+  //quantidade de filhos//
+  
+    printf("digite quantidade de filhos (0 se nao houver): ");
+  
+    scanf("%d",&tfilhos[p]);
+    
+    //habitantes com mais de 2 filhos//
+    
+    
+    if (tfilhos[p]>=2){
+        habit2fi++;
+        
+    }
+    
+ }
+     //calculo dos maiores salarios e menores//
+     
+      for (p=1; p<=3;p++){
+          
+            if (p == 1) {
+        maior = tsalario[1];
+        menor = tsalario[1];
+            }   
+       if (tsalario[p]>maior){
+           maior=tsalario[p];
+         
+       }
+          
+       else if (tsalario[p]<menor){
+           menor=tsalario[p];
+       }    
+          
+    
+          
+      }
+      
+      for(p=1; p<=3;p++){
+          
+          soma_salarios = soma_salarios + tsalario[p];
+          soma_filhos =soma_filhos + tfilhos[p];
+      }
+      
+      
+      
+
+      //percentual de pais de dois ou mais////////////////////////////////////////////////
+      
+    percentual = (habit2fi / 3.0) * 100;
+    mediasal =soma_salarios / 3.0;
+    mediaf=soma_filhos/3.0;
+//////os prints//////////////////////////////////////////////////////////////////
+
+    
+    
+     printf("MAIOR VALOR DE SALARIO:%f",maior);
+     printf("\nMENOR VALOR DE SALARIO :%f",menor);
+     printf("\n media salarial dos alunos: %f",mediasal);
+     printf("\n media do num de filhos dos alunos: %f",mediaf);
+     printf("\n percentagem de habitantes com 2 ou mais filhos: %d porcento",percentual);
+
+    return 0;
+}
